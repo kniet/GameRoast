3c1050dc570af9e025cc21c7abb2154fd3ecddc6
Jakub <k.nietupski99@gmail.com>
2024-01-19 14:44:28 +0100

    Basic routing in app added

--- client/GameRoastFrontend/src/app/app-constants.ts
+++ client/GameRoastFrontend/src/app/app-constants.ts
@@ -1,11 +1,11 @@
 export class AppConstants {
 
-//TODO podpiąć wszystkie linki przekierowania itd...
+//TODO podpiąć zaawansowane przekierowania (to już z backendem jbc)
 //TODO zrobić bazke i pokazac ziomalowi Adamowi
 //TODO zrobić backend
 //TODO podpiąć responsy z backendu do angulara (Jak zrobić zabezpieczenie przed wchodzeniem na linki??? np user do admin panelu - zabezpieczyć)
 //TODO testy manualne!!!
 
-  public static isAdmin: boolean | null = true;
+  public static isAdmin: boolean | null = false;
   public static gameScore: number = 10;
 }
